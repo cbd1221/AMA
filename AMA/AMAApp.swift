c2cbaad6aa7b7df1eca6d3c91435ab345f4df49e
Colin Dively <colinblairdively@gmail.com>
2023-01-02 20:40:10 -0500

    Initial Commit

---
+++ AMA/AMAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMAApp.swift
+//  AMA
+//
+//  Created by Colin Dively on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AMAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
